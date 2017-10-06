b697386656b44938aeeed7044368d20010bd7c01
rosyshrestha028 <rosyshrestha028@users.noreply.github.com>
2017-10-06 15:18:52 +0545

    add book page, create home as root route

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,5 +27,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/Materialize/dist/js/materialize.js')
   return app.toTree();
 };
