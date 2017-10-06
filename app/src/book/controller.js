eb24fbfcd4c736d24b8deafe19fc60ef6bd857ac
Abhinab Raj <abhinab.shady@hotmail.com>
2017-10-06 12:06:56 +0545

    detail page modified

--- app/src/book/controller.js
+++ app/src/book/controller.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+    isAvailable:false,
     modalIsOpen: false,
 
     actions: {
         modalfire: function() {
             this.toggleProperty('modalIsOpen');
+        },
+        agree: function(){
+            this.set('modalIsOpen', false);
+            Materialize.toast('Request has been sent', 4000);
+        },
+        closeModal: function(){
+            this.set('modalIsOpen', false);
         }
     }
 
