e9da560b12ca01694d8c5d5bfc4082a0599ad0a5
Amigo Die <divinemaniac@gmail.com>
2017-10-24 16:00:51 +0545

    update existing models and create bookrequest model

--- app/src/book/model.js
+++ app/src/book/model.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     imgurl: DS.attr('string'),
-    name:   DS.attr('string'),
+    name: DS.attr('string'),
     author: DS.attr('string'),
-    status: DS.attr('boolean',{ defaultValue: true }),
     description: DS.attr('string'),
-    requestby: DS.attr('string',{ defaultValue: null })
+    isAvailable: DS.attr('boolean'),
+    isBorrowed: DS.attr('boolean'),
 });
