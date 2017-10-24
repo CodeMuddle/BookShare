8eadbb631d2379c702d4e70921d26bf8c8dee29a
Abhinab <abhinab.rajopadhyaya@gmail.com>
2017-10-24 16:27:23 +0545

    Add default image and Request modal

--- app/src/book/model.js
+++ app/src/book/model.js
@@ -1,10 +1,24 @@
 import DS from 'ember-data';
+import computed from 'ember-computed';
 
 export default DS.Model.extend({
-    imgurl: DS.attr('string'),
+    imageURL: DS.attr('string'),
     name: DS.attr('string'),
     author: DS.attr('string'),
     description: DS.attr('string'),
     isAvailable: DS.attr('boolean'),
     isBorrowed: DS.attr('boolean'),
+})
+.reopen({
+    imgurl: computed('imageURL', {
+        get() {
+            if(this.get('imageURL')) return this.get('imageURL');
+            else return 'https://www.jordans.com/~/media/jordans%20redesign/no-image-found.ashx?h=275&la=en&w=275&hash=F87BC23F17E37D57E2A0B1CC6E2E3EEE312AAD5B';
+        },
+
+        set(key, value) {
+            this.set('imageURL', value);
+            return value;
+        }
+    })
 });
--- app/src/components/request-modal/component.js
+++ app/src/components/request-modal/component.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    isAccept:true,
+    isAccept:false,
+    isDecline:false,
 
     actions:{
         acceptReq(){
             console.log('request is accepted');
+            this.set('isAccept', true);
+        },
+        closeReqModal(){
+            this.sendAction('onCloseClick');
         }
+
     }
 });
